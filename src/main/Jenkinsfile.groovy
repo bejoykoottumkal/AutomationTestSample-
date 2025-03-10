2b7d8ba5587e0a53a312cdd09fcf4e3f1e54ff82
bejoykoottumkal <bejoykoottumkal@gmail.com>
2025-03-10 15:05:01 -0400

    Added Jenkinsfile for API automation

--- src/main/Jenkinsfile.groovy
+++ src/main/Jenkinsfile.groovy
@@ -1,1 +1,34 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout code from repository
+                git 'https://github.com/bejoykoottumkal/AutomationTestSample-.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // Build using Maven
+                sh 'mvn clean install'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                // Run TestNG tests
+                sh 'mvn test'
+            }
+        }
+
+        stage('Publish Results') {
+            steps {
+                // Publish TestNG test results
+                junit '**/target/test-classes/testng-results.xml'
+            }
+        }
+    }
+}
 
